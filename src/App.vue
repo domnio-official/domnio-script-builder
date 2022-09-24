8a1b3747572284c8b0da45a2c56a1fa4ed7c9a4d
windows-ubuntu <windows-ubuntu@libero.it>
2022-09-24 16:44:23 +0200

    Aggiunge robe

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,185 @@
 <script setup>
-    /**
-     * @license
-     * Copyright 2022 Google LLC
-     * SPDX-License-Identifier: Apache-2.0
-     */
-    
-    /**
-     * @fileoverview Main Vue component that includes the Blockly component.
-     * @author dcoodien@google.com (Dylan Coodien)
-     */
-    
-    import { ref } from "vue";
-    import BlocklyComponent from "./components/BlocklyComponent.vue";
-    import "./blocks/stocks";
-    
-    import BlocklyJS from "blockly/javascript";
-    
-    const foo = ref();
-    const code = ref();
-    const options = {
-      media: "media/",
-      grid: {
-        spacing: 25,
-        length: 3,
-        colour: "#ccc",
-        snap: true,
-      },
-      toolbox: `<xml>
-              <category name="Logic" colour="%{BKY_LOGIC_HUE}">
-                <block type="controls_if"></block>
-                <block type="logic_compare"></block>
-                <block type="logic_operation"></block>
-                <block type="logic_negate"></block>
-                <block type="logic_boolean"></block>
-              </category>
-              <category name="Loops" colour="%{BKY_LOOPS_HUE}">
-                <block type="controls_repeat_ext">
-                  <value name="TIMES">
-                    <block type="math_number">
-                      <field name="NUM">10</field>
-                    </block>
-                  </value>
-                </block>
-                <block type="controls_whileUntil"></block>
-              </category>
-              <category name="Math" colour="%{BKY_MATH_HUE}">
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Main Vue component that includes the Blockly component.
+ * @author dcoodien@google.com (Dylan Coodien)
+ */
+
+import { ref } from "vue";
+import BlocklyComponent from "./components/BlocklyComponent.vue";
+import "./blocks/stocks";
+import Theme from '@blockly/theme-dark';
+
+import BlocklyJS from "blockly/javascript";
+
+const foo = ref();
+const code = ref();
+const options = {
+  media: "./media/",
+  grid: {
+    spacing: 25,
+    length: 3,
+    colour: "#ccc",
+    snap: true,
+  },
+  renderer: "zelos",
+  theme: Theme,
+  toolbox: `<xml>
+          <category name="Logic" colour="%{BKY_LOGIC_HUE}">
+            <block type="controls_if"></block>
+            <block type="logic_compare"></block>
+            <block type="logic_operation"></block>
+            <block type="logic_negate"></block>
+            <block type="logic_boolean"></block>
+          </category>
+          <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+            <block type="controls_repeat_ext">
+              <value name="TIMES">
                 <block type="math_number">
-                  <field name="NUM">123</field>
+                  <field name="NUM">10</field>
                 </block>
-                <block type="math_arithmetic"></block>
-                <block type="math_single"></block>
-              </category>
-              <category name="Text" colour="%{BKY_TEXTS_HUE}">
-                <block type="text"></block>
-                <block type="text_length"></block>
-                <block type="text_print"></block>
-              </category>
-              <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
-                </category>
-              <category name="Stocks" colour="%{BKY_LOOPS_HUE}">
-                <block type="stock_buy_simple"></block>
-                <block type="stock_buy_prog"></block>
-                <block type="stock_fetch_price"></block>
-              </category>
-            </xml>`,
-    };
-    
-    const showCode = () => (code.value = BlocklyJS.workspaceToCode(foo.value.workspace));
-    </script>
-    
-    <template>
-      <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <BlocklyComponent id="blockly1">
-          <block type="controls_ifelse"></block>
-          <block type="logic_compare"></block>
-          <block type="logic_operation"></block>
-          <block type="controls_repeat_ext">
-            <value name="TIMES">
-              <shadow type="math_number">
-                <field name="NUM">10</field>
-              </shadow>
-            </value>
-          </block>
-          <block type="logic_operation"></block>
-          <block type="logic_negate"></block>
-          <block type="logic_boolean"></block>
-          <block type="logic_null" disabled="true"></block>
-          <block type="logic_ternary"></block>
-          <block type="text_charAt">
-            <value name="VALUE">
-              <block type="variables_get">
-                <field name="VAR">text</field>
-              </block>
-            </value>
-          </block>
-        </BlocklyComponent>
-    
-        <BlocklyComponent id="blockly2" :options="options" ref="foo"></BlocklyComponent>
-        <p id="code">
-          <button v-on:click="showCode()">Show JavaScript</button>
-          <pre v-html="code"></pre>
-        </p>
+              </value>
+            </block>
+            <block type="controls_whileUntil"></block>
+          </category>
+          <category name="Math" colour="%{BKY_MATH_HUE}">
+            <block type="math_number">
+              <field name="NUM">123</field>
+            </block>
+            <block type="math_arithmetic"></block>
+            <block type="math_single"></block>
+          </category>
+          <category name="Text" colour="%{BKY_TEXTS_HUE}">
+            <block type="text"></block>
+            <block type="text_length"></block>
+            <block type="text_print"></block>
+          </category>
+          <category name="Variables" custom="VARIABLE" colour="%{BKY_VARIABLES_HUE}">
+            </category>
+          <category name="Stocks" colour="%{BKY_LOOPS_HUE}">
+            <block type="stock_buy_simple"></block>
+            <block type="stock_buy_prog"></block>
+            <block type="stock_fetch_price"></block>
+          </category>
+        </xml>`,
+};
+
+const showCode = () => (code.value = BlocklyJS.workspaceToCode(foo.value.workspace));
+
+function copy() {
+  var copyText = document.getElementById("codeee").innerText;
+    navigator.clipboard.writeText(copyText)
+}
+</script>
+
+<template>
+
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(22, 23, 25);">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./assets/logo.png" alt="Logo" width="30" draggable="false" class="d-inline-block align-text-top">
+      <b> Domnio Script Maker</b>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#codeModal" v-on:click="showCode()">Export to Javascript</button>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="https://dsc.gg/domnio" target="_blank">Discord server</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu bg-white">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success text-white" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="modal fade modal-xl modal-dialog-scrollable" id="codeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header bg-black">
+        <h5 class="modal-title text-white" id="exampleModalLabel">Code</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-    </template>
-    
-    <style>
-    #app {
-      font-family: "Avenir", Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      color: #2c3e50;
-    }
-    
-    html,
-    body {
-      margin: 0;
-    }
-    
-    #code {
-      position: absolute;
-      right: 0;
-      bottom: 0;
-      width: 50%;
-      height: 50%;
-      margin: 0;
-      background-color: beige;
-    }
-    
-    #blockly1 {
-      position: absolute;
-      right: 0;
-      top: 0;
-      width: 50%;
-      height: 50%;
-    }
-    
-    #blockly2 {
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 50%;
-      height: 50%;
-    }
-    </style>
+      <div class="modal-body bg-black">
+        <pre v-html="code" class="text-white" id="codeee"></pre>
+      </div>
+      <div class="modal-footer bg-black">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" v-on:click="copy()" data-bs-dismiss="modal" id="liveToastBtn">Copy</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <BlocklyComponent id="workspace" :options="options" ref="foo"></BlocklyComponent>
+    <p id="code">
+      <button v-on:click="showCode()">Show JavaScript</button>
+    </p>
+  </div>
+</template>
+
+<style>
+
+@font-face {
+  font-family: "Ubuntu";
+  src: url(./fonts/Ubuntu.ttf);
+}
+
+a {
+  font-family: "Ubuntu", sans-serif;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+html,
+body {
+  margin: 0;
+}
+
+#app {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  background-color: black;
+}
+
+#workspace {
+  position: absolute;
+    width: 100%;
+    height: calc(100% - 59px);
+}
+
+/* .navbar-expand-lg {
+  min-height: 6.5%;
+} */
+</style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
- import { createApp } from "vue";
- import App from "./App.vue";
- 
- const app = createApp(App);
- 
- app.mount("#app");
+// import { BootstrapVue } from 'bootstrap-vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+/*import VueToast from 'vue-toast-notification';
+import VueTour from 'vue-tour'; */
+
+const app = createApp(App);
+
+app.mount("#app");
+
+/* import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-tour/dist/vue-tour.css'; */

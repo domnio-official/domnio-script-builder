af6596d2866dba2d05892641f6c49ac61681b5b6
windows-ubuntu <windows-ubuntu@libero.it>
2022-09-25 13:28:53 +0200

    r

---
+++ src/components/save-load.vue
@@ -0,0 +1,20 @@
+<script>
+import { ref } from "vue";
+import BlocklyComponent from "./BlocklyComponent.vue";
+import './importBlocks';
+import Theme from '@blockly/theme-dark';
+import Swal from "sweetalert2";
+import JSZip from "jszip";
+import beautify from "js-beautify";
+import localforage from "localforage";
+import Blockly from "blockly";
+import BlocklyJS from "blockly/javascript";
+
+const foo = ref(window.parent.Document.getElementById("workspace"));
+
+export default {
+    methods: {
+
+    }
+}
+</script>

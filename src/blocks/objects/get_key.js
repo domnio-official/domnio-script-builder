39250005bd0872b43896c90377e3056dafbe2d7f
windows-ubuntu <windows-ubuntu@libero.it>
2022-10-08 19:49:02 +0200

    better object

--- src/blocks/objects/get_key.js
+++ src/blocks/objects/get_key.js
@@ -21,10 +21,13 @@ Blockly.Blocks['get_obj_key'] = {
   Blockly.JavaScript['get_obj_key'] = function(block) {
     var value_obj_name = Blockly.JavaScript.valueToCode(block, 'obj_name', Blockly.JavaScript.ORDER_ATOMIC);
     var value_key_name = Blockly.JavaScript.valueToCode(block, 'key_name', Blockly.JavaScript.ORDER_ATOMIC);
-    value_key_name = value_key_name.slice(0, -1);
-    value_key_name = value_key_name.replace('\'', '');
-    // TODO: Assemble JavaScript into code variable.
-    var code = `${value_obj_name}.${value_key_name}`;
-    // TODO: Change ORDER_NONE to the correct strength.
+    if (value_key_name.startsWith("'")) {
+      value_key_name = value_key_name.slice(0, -1);
+      value_key_name = value_key_name.replace('\'', '');
+      var code = `${value_obj_name}.${value_key_name}`;
+    }
+    else {
+      var code = `${value_obj_name}[String(${value_key_name})]`;
+    }
     return [code, Blockly.JavaScript.ORDER_NONE];
   };
